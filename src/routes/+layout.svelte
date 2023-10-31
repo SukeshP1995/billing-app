d68dd3771fb093adbd52f7499ccab948acfb8ba9
SukeshP1995 <30713956+SukeshP1995@users.noreply.github.com>
2023-10-31 07:49:02 +0100

    Layout with Tollbar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import TopAppBar, { Row, Section, Title } from '@smui/top-app-bar';
+  import IconButton from '@smui/icon-button';
+  import Button, { Label } from '@smui/button';
+
+</script>
+
+<TopAppBar
+  variant="static"
+  color={'primary'}
+>
+  <Row>
+    <Section>
+      <Title>Billing App</Title>
+    </Section>
+    <Section align="end" toolbar>
+      <Button href="/admin">
+        <Label>admin</Label>
+      </Button>
+      <IconButton class="material-icons" aria-label="Home" href='/'>
+        home
+      </IconButton>
+    </Section>
+  </Row>
+</TopAppBar>
+<slot />
